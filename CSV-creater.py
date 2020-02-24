6fec507c70dbe4c3f6e0b00e849fda0980a354ab
biswazr <biswazr@users.noreply.github.com>
2020-02-25 01:04:46 +0530

    Add files via upload

--- CSV-creater.py
+++ CSV-creater.py
@@ -1,1 +1,15 @@
-
+import os.path
+path = './DENV1/'
+#isdir = os.path.isdir(path) 
+#subdirs = [x[0] for x in os.walk(path)]
+##print(subdirs)
+dir_list = os.listdir(path)
+##print(dir_list)
+isDirectory = os.path.isdir(path)
+##print(isDirectory)
+for each_dir in dir_list:
+    #print(each_dir)
+    b = os.path.isdir(path+each_dir)
+    if b :
+        print(each_dir)
+    #print(b)
