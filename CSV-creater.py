8bbed175ce5aeddc2d6d244a84d94b53d7bf8ad1
biswazr <dev@eraofgame.com>
2020-02-26 20:51:53 +0530

    new files created to track

--- CSV-creater.py
+++ CSV-creater.py
@@ -1,4 +1,5 @@
-import os.path
+import os.path 
+import csv
 path = './DENV1/'
 #isdir = os.path.isdir(path) 
 #subdirs = [x[0] for x in os.walk(path)]
@@ -12,4 +13,29 @@ for each_dir in dir_list:
     b = os.path.isdir(path+each_dir)
     if b :
         print(each_dir)
-    #print(b)
+        
+
+#for file in os.listdir((each_dir)):
+    #if file.endswith(".txt"):
+        #print(os.path.join((each_dir), file))
+
+import pathlib
+for txt_file in pathlib.Path('(each_dir)').glob('**/*.txt'):
+    if file.endswith(".txt"):
+        print(os.path.join((each_dir), file))
+            
+
+    #with open('data1.csv', 'w', newline='') as file:
+    #    writer = csv.writer(file)
+    #    writer.writerow(["each_dir"])
+        #open (each_dir)
+        #open(log.txt)
+        #print(log.txt)
+    
+
+
+
+
+
+
+    ## https://thispointer.com/python-how-to-get-list-of-files-in-directory-and-sub-directories/ ##
